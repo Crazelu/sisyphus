cf04487adbdf97f30bdda3b584babd47fbd932d0
Crazelu <eberelucky64@gmail.com>
2023-06-04 01:20:44 +0100

    chore: adds app icon assets and fonts

---
+++ lib/presentation/app_assets.dart
@@ -0,0 +1,19 @@
+class AppAssets {
+  static String _getAssetPath(String asset) {
+    return "assets/icons/$asset.png";
+  }
+
+  static String searchIcon = _getAssetPath("search");
+  static String avatar = _getAssetPath("avatar");
+  static String btc = _getAssetPath("btc");
+  static String usdt = _getAssetPath("usdt");
+  static String clock = _getAssetPath("clock");
+  static String expand = _getAssetPath("expand");
+  static String globe = _getAssetPath("globe");
+  static String logo = _getAssetPath("logo");
+  static String menu = _getAssetPath("menu");
+  static String dropDown = _getAssetPath("drop_down");
+  static String arrowUp = _getAssetPath("arrow_up");
+  static String arrowDown = _getAssetPath("arrow_down");
+  static String candleChart = _getAssetPath("candle_chart");
+}
